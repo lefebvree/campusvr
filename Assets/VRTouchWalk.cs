aa64400886ff5af6fb5591d4d22575e8c4f085a5
arnaudsoulier <soulier.arnaud@hotmail.fr>
2017-03-03 22:41:16 +0100

    add Touch Script

--- Assets/VRTouchWalk.cs
+++ Assets/VRTouchWalk.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VRLookWalk : MonoBehaviour {
+public class VRTouchWalk : MonoBehaviour {
 
 	// VR Main Camera
 	public Transform vrCamera;
 
-	// ANgle at witch walk/stop will be triggered (X value of main camera)
-	public float toggleAngle = 25.0f;
 	// How fast to move
 	public float speed = 15.0f;
-
-	// Should I move forward or not
-	public bool moveForward;
 
 	// CharacterController script
 	private CharacterController cc;
@@ -26,16 +21,7 @@ public class VRLookWalk : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f) {
-			// Move forward
-			moveForward = true;
-		} else {
-			// Stop moving
-			moveForward = false;
-		}
-
-		// Check to see if I should move
-		if (moveForward) {
+		foreach(Touch touch in Input.touches) {
 			// Find the forward direction
 			Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
 			// Tell CharacterController to move forward
