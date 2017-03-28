1b4a15fecda3b93f52783afaf85318f816bbb261
Etienne Lefebvre <etiennelefebvre@outlook.com>
2017-03-28 11:22:40 +0200

    reorganized scene and assets structure, lookedAt script, envent system controller

---
+++ Assets/Scripts/lookedAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookedAt : MonoBehaviour {
+	
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+	void Start () {
+		SetGazedAt(false);
+	}
+	
+	public void SetGazedAt(bool gazedAt) {
+    if (inactiveMaterial != null && gazedAtMaterial != null) {
+		GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+		return;
+    }
+    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+  }
+}
