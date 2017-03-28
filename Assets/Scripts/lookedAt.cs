e547736599de48c482afe1dc8b21cc33e1c9f35f
Etienne Lefebvre <etiennelefebvre@outlook.com>
2017-03-28 12:29:09 +0200

    add gaze script to buildings

--- Assets/Scripts/lookedAt.cs
+++ Assets/Scripts/lookedAt.cs
@@ -6,16 +6,21 @@ public class lookedAt : MonoBehaviour {
 	
 	public Material inactiveMaterial;
 	public Material gazedAtMaterial;
+	public Canvas buildingInfos;
 
 	void Start () {
 		SetGazedAt(false);
 	}
 	
 	public void SetGazedAt(bool gazedAt) {
-    if (inactiveMaterial != null && gazedAtMaterial != null) {
-		GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-		return;
-    }
-    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
-  }
+		if (inactiveMaterial != null && gazedAtMaterial != null) {
+			GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+		} else {
+			GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		}
+		
+		if (buildingInfos != null) {
+			buildingInfos.GetComponent<CanvasRenderer>().SetAlpha(gazedAt ? 1f : 0f);
+		}    
+	}
 }
