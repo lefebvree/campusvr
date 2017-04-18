838940e02a8a0fa9d1e4eea4932aafdc151e86e5
Etienne Lefebvre <etiennelefebvre@outlook.com>
2017-04-18 11:52:47 +0200

    create specific teleport script, fixed application name, adjust speed

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -19,15 +19,13 @@ public class VRMovement : MonoBehaviour {
 	public Transform vrHead;
 
 	// How fast to move
-	public float speed = 15.0f;
+	public float speed = 10.0f;
 
 	// Should I move forward or not
 	public bool moveForward;
 	
 	// Angle at witch walk/stop will be triggered (X value of main camera)
 	public float toggleAngle = 25.0f;
-	
-	public Dropdown teleportOption;
 
 	
 	// Use this for initialization
@@ -43,28 +41,6 @@ public class VRMovement : MonoBehaviour {
 		if(movementType == maxMovementType) {
 			movementType = 0;
 		}
-	}
-
-	public void Teleport() {
-		int option = teleportOption.value;
-		
-		switch(option) {
-			case 1:
-				// Entrance
-				transform.position = new Vector3(543, 0, 102);
-				break;
-			case 2:
-				// BU
-				transform.position = new Vector3(572, 0, 330);
-				break;
-			case 3:
-				// B.5
-				transform.position = new Vector3(407, 0, 464);
-				break;
-		}
-		
-		// Reset selected value
-		teleportOption.value = 0;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/teleportTo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class teleportTo : MonoBehaviour {
+	
+	public Dropdown teleportOption;
+
+	public void Teleport() {
+		int option = teleportOption.value;
+		
+		switch(option) {
+			case 1:
+				// Entrance
+				transform.position = new Vector3(543, 0, 102);
+				break;
+			case 2:
+				// BU
+				transform.position = new Vector3(572, 0, 330);
+				break;
+			case 3:
+				// B.5
+				transform.position = new Vector3(407, 0, 464);
+				break;
+		}
+		
+		// Reset selected value
+		teleportOption.value = 0;
+	}
+}
